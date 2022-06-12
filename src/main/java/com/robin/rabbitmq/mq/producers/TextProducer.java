797914b3b5e7b460104c35fae1a8e7d4275318ba
ParkRobin <lpiao@ncsu.edu>
2022-06-12 17:32:01 -0400

    feat: send message to default exchange

--- src/main/java/com/robin/rabbitmq/mq/producers/TextProducer.java
+++ src/main/java/com/robin/rabbitmq/mq/producers/TextProducer.java
@@ -2,7 +2,6 @@ package com.robin.rabbitmq.mq.producers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -23,12 +22,22 @@ public class TextProducer {
     @Autowired
     private Exchange textTopicExchange;
 
+    public boolean sendMessageToDefaultExchange(String queueName, String message){
+        try{
+            rabbitTemplate.convertAndSend("", queueName, message);
+            return true;
+        }catch (Exception e){
+            log.error("Send Message To Default Exchange Error, queueName: {}, message: {}", queueName, message, e);
+            return false;
+        }
+    }
+
     public boolean sendMessageToDirectExchange(String routingKey, String message){
         try{
             rabbitTemplate.convertAndSend(textDirectExchange.getName(), routingKey, message);
             return true;
         }catch (Exception e){
-            System.out.println(e);
+            log.error("Send Message To Direct Exchange Error, routingKey: {}, message: {}", routingKey, message, e);
             return false;
         }
     }
@@ -38,7 +47,7 @@ public class TextProducer {
             rabbitTemplate.convertAndSend(textFanoutExchange.getName(), "", message);
             return true;
         }catch (Exception e){
-            System.out.println(e);
+            log.error("Send Message To Fanout Exchange Error, message: {}", message, e);
             return false;
         }
     }
@@ -48,7 +57,7 @@ public class TextProducer {
             rabbitTemplate.convertAndSend(textTopicExchange.getName(), routingKey, message);
             return true;
         }catch (Exception e){
-            System.out.println(e);
+            log.error("Send Message To Topic Exchange Error, routingKey: {}, message: {}", routingKey, message, e);
             return false;
         }
     }

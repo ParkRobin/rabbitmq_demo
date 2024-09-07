9b072d5c1684d498e535e884fd467061666fc91a
ParkRobin <lpiao@ncsu.edu>
2024-09-07 10:04:27 -0400

    update: Spring boot 3

--- src/main/java/com/robin/rabbitmq/MainApplication.java
+++ src/main/java/com/robin/rabbitmq/MainApplication.java
@@ -1,8 +1,19 @@
 package com.robin.rabbitmq;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "RabbitMQ Demo API",
+                version = "1.0",
+                description = "RabbitMQ Demo project",
+                contact = @Contact(name = "Robin Park", url = "https://github.com/parkrobin", email = "lpiao@ncsu.edu")
+        )
+)
 @SpringBootApplication
 public class MainApplication {
 
